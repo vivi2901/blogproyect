02c58d1eea343696a3ce87a9e7a4361c096d8076
vivi2901 <202000200@est.umss.edu>
2025-03-28 12:14:24 -0400

    blogPage

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -10,7 +10,6 @@ export default async function BlogPage({ params }: { params: Promise<{ id: strin
       <h2 className="inline-flex gap-2 text-lg font-bold">
         <span>{blog.title}</span>
         <small className="inline-flex gap-1">
-          <span>⭐</span>
           <span>{blog.views_count}</span>
           <span className="font-normal opacity-75">({blog.comments_count})</span>
         </small>
