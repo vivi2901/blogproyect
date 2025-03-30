1c4a76270c189de2dd1ac74dd5a7cf0b6bba12e2
vivi2901 <202000200@est.umss.edu>
2025-03-30 19:10:25 -0400

    errorMessageUI

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,27 @@
 'use client';
 
-export default function ErrorPage({ error }: { error: Error }) {
-  console.error(error);
+import { useEffect } from 'react';
+import Link from 'next/link';
 
-  return <div>Algo salió mal, ¡inténtalo de nuevo!</div>;
+export default function ErrorPage({ error }: { error: Error }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-6">
+        <h2 className="text-2xl font-semibold text-red-800">¡Algo salió mal!</h2>
+        <p className="mt-4 mb-4 text-gray-700">
+          Lo sentimos, ha ocurrido un error inesperado. Por favor, inténtalo de nuevo más tarde.
+        </p>
+        <Link
+          href={`/`}
+          className="bg-red-800 text-white py-2 px-4 rounded-md hover:bg-red-900 transition-colors"
+        >
+          Regresar
+        </Link>
+      </div>
+    </div>
+  );
 }
